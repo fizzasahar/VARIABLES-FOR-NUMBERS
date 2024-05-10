093b79ce47033cc2abd7b0f63739736b47301033
fizzasahar <fizzasahar44>
2024-05-10 13:09:24 +0500

    variable for number

---
+++ app.js
@@ -0,0 +1,13 @@
+var age = 25; 
+alert("I am 15 Year old" ); 
+var visits = 1; 
+alert("You have visited this site 14 times"); 
+var birthYear = 1990; 
+document.write("My birth year is " + birthYear + "." + "<br/>"); 
+document.write("Date type of my declared variable is number."+ "<br/>" );
+var visitorName = "John Doe"; 
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store."); 
+var message = "This is a custom alert message.";
+alert('<div style="border: 2px solid black; padding: 10px;">' + message + '</div>');
